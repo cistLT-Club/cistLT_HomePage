a38b3ab4f3259047b3fa1ac4c7c0e0e1e5541960
YoshikiYamada <yoshikiyamada@usernoMacBook-puro.local>
2020-08-04 22:19:29 +0900

    google analyticsの追加 log分析を可能にした

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "637908496727",
+  appId: "2:637908496727:web:a4284b4c99e329d5",
+  measurementId: "G-9VP01NDSXJ"
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './static/css/application.scss'
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+//Google Analytics
+ReactGA.initialize('UA-173647357-1');
+const history = createBrowserHistory();
+history.listen(({ pathname }) => {
+  ReactGA.set({ page: pathname });
+  ReactGA.pageview(pathname);
+});
 
 ReactDOM.render(
   <React.StrictMode>
