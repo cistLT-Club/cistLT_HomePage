45104c129423027333731a2638775226c1b50cde
YoshikiYamada <yoshikiyamada@usernoMacBook-puro.local>
2020-10-20 22:46:08 +0900

    画像の軽量化 imgタグに少々手間を加えました

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -7,7 +7,7 @@ const BlogCard =(props)=>{
     <>
     <Link to={props.url} className='blogCard'>
       
-        <img src={props.data.fields.image.fields.file.url}/>
+        <img src={`${props.data.fields.image.fields.file.url}?&w=100&h=200`}/>
         <h3>{props.data.fields.title}</h3>
         <p>{props.data.fields.publishDate}</p>
      
--- src/components/MembersCard.js
+++ src/components/MembersCard.js
@@ -5,7 +5,7 @@ const MembersCard =(props)=>{
   return(
     <>
     <div className='membersCard'>
-      <img src={props.data.fields.image.fields.file.url}/>
+      <img src={`${props.data.fields.image.fields.file.url}?&w=100&h=200`}/>
       <p>{props.data.fields.name}</p>
     </div>
     </>
